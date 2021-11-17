ce8b5e0101b20f1d280fb520ca895c90c5a9d05c
Cláudio Oliveira <83782979+claudio1994-oliveira@users.noreply.github.com>
2021-11-17 15:51:43 -0300

    Add files via upload

---
+++ database/migrations/2021_11_17_000353_criar_tabela_usuarios.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CriarTabelaUsuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email')->unique();
+            $table->string('password');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
